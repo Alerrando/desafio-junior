0ede8807a76317db4bd7a736d421840e96053969
Alerrando <alerrando2@gmail.com>
2024-03-16 13:19:34 -0300

    feat: add event in result button

--- calculator/script.js
+++ calculator/script.js
@@ -8,3 +8,20 @@ for (let index = 0; index < buttonCalc.length; index++) {
         inputCalculator.value += e.target.innerHTML;
     })
 }
+
+buttonResult.addEventListener("click", () => {
+    try {
+        const result = eval(inputCalculator.value);
+        inputCalculator.value = result;
+        
+    } catch (error) {
+        const toastContainer = document.querySelector(".toast-container");
+        const toastMessage = document.querySelector(".toast-message");
+        toastMessage.innerHTML = "Conta invalida";
+        toastContainer.classList.add("true");
+
+        setTimeout(() => {
+            toastContainer.classList.remove("true");
+        }, 5000)
+    }
+});
