92d91249a8c45ac163426bb5446db0a3c5efa352
Alerrando <alerrando2@gmail.com>
2024-03-23 13:57:42 -0300

    feat: add calculations to simulate the investment

--- financing-simulator/script.js
+++ financing-simulator/script.js
@@ -1,20 +1,28 @@
 const form = document.querySelector("form");
 const range = document.querySelector(".form-range");
 
-range.addEventListener("click", (e) => {
-    const spanRange = document.querySelector("#quantPortion");
-    spanRange.innerHTML = `Quantidade de Parcelas: ${e.target.value}`;
-})
-
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    calcQuantPortion(parseFloat(event.target[0].value), parseInt(event.target[1].value), parseInt(event.target[2].value));
+    calcFinancing(parseFloat(event.target[0].value), parseInt(event.target[1].value), parseInt(event.target[2].value));
 })
 
-function calcQuantPortion(valueTotal, quantPortion, nominalInterestRate) {
-    const aux = Math.pow((1 + nominalInterestRate), -quantPortion);
-    const portion = valueTotal * (nominalInterestRate / (1 - aux));
+function calcFinancing(valueTotal, quantPortion, nominalInterestRate) {
+    const valueRange = document.querySelector("#valueRange");
+    const totalAmount = document.querySelector("#valueTotalFinancement");
+    const totalEffectiveCost = document.querySelector("#totalEffectiveCostFinancing");
+    const effectiveMonthlyRate = document.querySelector("#effectiveMonthlyRate");
 
-    console.log(portion.toFixed(2));
+    const nominalInterestRateMonth = (nominalInterestRate / 100) / 12;
+    const portion = valueTotal * (nominalInterestRateMonth / (1 - Math.pow((1 + nominalInterestRateMonth), -quantPortion)));
+
+    valueRange.innerHTML = `R$${portion.toFixed(2)}`;
+    totalAmount.innerHTML = `R$${(portion * quantPortion).toFixed(2)}`
+    totalEffectiveCost.innerHTML = `R$${((portion * quantPortion) - valueTotal).toFixed(2)}`
+    effectiveMonthlyRate.innerHTML = `${(Math.pow((1 + nominalInterestRateMonth), (1/12)) - 1).toFixed(5)}%`
+}
+
+function showQuantPortion(e){
+    const spanRange = document.querySelector("#quantPortion");
+    spanRange.innerHTML = `Quantidade de Parcelas: ${e.value}`;
 }
