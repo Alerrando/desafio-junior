7c39906025dade445345f2404c886813d2cc4ba2
Alerrando <alerrando2@gmail.com>
2024-03-24 13:01:39 -0300

    feat: add demo to to-do and add confirmation delete to to-do

--- 6 - to-do/script.js
+++ 6 - to-do/script.js
@@ -63,10 +63,10 @@ function toggleButtonStatus(e) {
 }
 
 function handleDelete(id) {
-  todoArray = todoArray.filter(todo => todo.id !== id);
-  modal = false;
-
-  renderTable();
+  if(window.confirm(`Tem certeza que deseja excluir a tarefa ${todoArray.find(todo => todo.id === id).description}?`)) {
+    todoArray = todoArray.filter(todo => todo.id !== id);
+    renderTable();
+  }
 }
 
 function isDescriptionUnique(description) {
@@ -116,9 +116,7 @@ function addToDoInTable(todo) {
   });
 
   const td = document.createElement('td');
-  td.classList.add("delete-button");
-  td.setAttribute("data-id", todo.id);
-  td.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  td.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash delete-button" data-id="${todo.id}" viewBox="0 0 16 16">
       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
       <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
       </svg>
