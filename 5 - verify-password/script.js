c916364f4c9fc1767233bd33072583fdedb5a141
Alerrando <alerrando2@gmail.com>
2024-03-24 12:20:20 -0300

    feat: add demo of verify-password project fix: fixing error in js of verify-password

--- 5 - verify-password/script.js
+++ 5 - verify-password/script.js
@@ -5,27 +5,39 @@ passwordInput.addEventListener("keyup", (e) => {
 })
 
 function verifyPassword(e){
-    if(e.value === 0){
-        e.style.border = "1px solid #fff"
+    if(e.value.length === 0){
+        e.style.border = "1px solid #fff";
+        return;
     }
 
-    if(e.value.length < 8 || /^[A-Z]*$/.test(e.value) || /^[a-z]*$/.test(e.value) || !/.*[0-9].*/.test(e.value)){
+    if(e.value.length < 8 || !/[A-Z]/.test(e.value) || !/[a-z]/.test(e.value) || !/\d/.test(e.value)){
         e.style.border = "2px solid #f00";
-        return true;
+    } else {
+        e.style.border = "1px solid #fff";
     }
-    
-    e.style.border = "1px solid #fff"
 }
 
 function handleButton(){
+    if(e.value.length === 0){
+        const toastContainer = document.querySelector(".toast-container");
+        const toastMessage = document.querySelector(".toast-message");
+        toastMessage.innerHTML = "Por favor, insira uma senha.";
+        toastContainer.classList.add("true");
+
+        setTimeout(() => {
+            toastContainer.classList.remove("true");
+        }, 5000);
+        return;
+    }
+
     if(verifyPassword(passwordInput)){
         const toastContainer = document.querySelector(".toast-container");
         const toastMessage = document.querySelector(".toast-message");
-        toastMessage.innerHTML = "A senha tem que ter no mínimo 8 caracteres, pelo menos uma letra maiúscula, uma letra minuscula e um número";
+        toastMessage.innerHTML = "A senha deve ter no mínimo 8 caracteres, pelo menos uma letra maiúscula, uma letra minúscula e um número.";
         toastContainer.classList.add("true");
-    
+
         setTimeout(() => {
             toastContainer.classList.remove("true");
-        }, 5000)
+        }, 5000);
     }
 }
