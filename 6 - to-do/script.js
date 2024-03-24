2e21c79d8418477d75b8e9334d10a5b06348af25
Alerrando <alerrando2@gmail.com>
2024-03-24 12:56:03 -0300

    chang: changing css to to-do

--- 6 - to-do/script.js
+++ 6 - to-do/script.js
@@ -32,6 +32,8 @@ function handleSubmitForm(e) {
     const newTodo = { id, description, priority, status: true };
     todoArray.push(newTodo);
     renderTable();
+    e.target[0].value = "";
+    containerModal.classList.remove('true');
   } else {
     window.alert("Tarefa já cadastrada");
   }
@@ -62,6 +64,8 @@ function toggleButtonStatus(e) {
 
 function handleDelete(id) {
   todoArray = todoArray.filter(todo => todo.id !== id);
+  modal = false;
+
   renderTable();
 }
 
@@ -112,7 +116,9 @@ function addToDoInTable(todo) {
   });
 
   const td = document.createElement('td');
-  td.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash delete-button" data-id="${todo.id}" viewBox="0 0 16 16">
+  td.classList.add("delete-button");
+  td.setAttribute("data-id", todo.id);
+  td.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
       <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
       </svg>
